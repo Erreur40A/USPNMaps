5c5c353029d7b8ad96aafb6e369688fc9e9d3719
Medifc <medi.hsito22@gmail.com>
2025-10-08 12:34:01 +0200

    fgyuio

---
+++ app/src/main/java/com/uspn/maps/Salle.kt
@@ -0,0 +1,12 @@
+package com.uspn.maps
+
+package com.uspn.maps
+
+import org.osmdroid.util.GeoPoint
+
+data class Salle(
+    val nom: String,
+    val etage: Int,
+    val description: String,
+    val position: GeoPoint
+)
